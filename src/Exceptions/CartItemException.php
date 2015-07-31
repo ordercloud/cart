9e002419565092b723d14d8a570c8c4727fe8766
Michael Nel <michael.a.nel1@gmail.com>
2015-07-31 09:51:04 +0200

    Updated CartItemException

--- src/Exceptions/CartItemException.php
+++ src/Exceptions/CartItemException.php
@@ -1,27 +1,31 @@
 <?php namespace Ordercloud\Cart\Exceptions;
 
 use Exception;
+use Ordercloud\Cart\Entities\Cart;
 use Ordercloud\Cart\Entities\CartItem;
 
-class CartItemException extends Exception
+class CartItemException extends CartException
 {
     /**
-     * @var CartItem
+     * @var CartItem|null
      */
     private $item;
 
     /**
-     * @param string   $message
-     * @param CartItem $item
+     * @param CartItem|null  $item
+     * @param Cart|null      $cart
+     * @param string         $message
+     * @param int            $code
+     * @param Exception|null $previous
      */
-    public function __construct($message, CartItem $item)
+    public function __construct(CartItem $item = null, Cart $cart = null, $message = '', $code = 0, Exception $previous = null)
     {
-        parent::__construct($message);
+        parent::__construct($cart, $message, $code, $previous);
         $this->item = $item;
     }
 
     /**
-     * @return CartItem
+     * @return CartItem|null
      */
     public function getItem()
     {
